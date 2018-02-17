0f6b59367f04f3453283981254d374da474602bb
Jared Robertson <jared.robertson@usask.ca>
2018-02-17 14:44:51 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-      to: 'Jen@example.com',
-      text: "Hey, this is Jared.",
-    })
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Mike@example.com',
-        text: "Hey, what's up?",
-        createdAt: 123
-    });
-
-    socket.on('createMessage', (newMessage) => {
-        console.log('createEmail', newMessage);
+    socket.on('createMessage', (message) => {
+        console.log('createEmail', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 
     socket.on('disconnect', () => {
