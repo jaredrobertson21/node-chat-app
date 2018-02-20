8fb937362ed237a9fe5b5cdab6084b06a2f7fcd8
Jared Robertson <jared.robertson@usask.ca>
2018-02-19 18:00:41 -0600

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -10,17 +10,19 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   console.log('New message', message);
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
 
     li.append(a);
@@ -45,7 +47,7 @@ locationButton.on('click', function() {
         return alert('Geolocation is not supported by your browser.');
     }
 
-    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+    locationButton.attr('disabled', 'disabled').text('Sending Location... ');
 
     navigator.geolocation.getCurrentPosition(function(position) {
         locationButton.removeAttr('disabled').text('Send Location');
